d68e0f4cec62d9b4358192738f84b3d28b9b5710
sheshankp <sheshank3336@gmail.com>
2022-07-06 15:01:04 +0530

    part 4 started

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import { Container } from "@mui/material";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { BrowserRouter, Route, Switch, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route exact path="/auth" element={<Auth />} />
+          <Route path="/" element={<Navigate to="/posts" replace />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route
+            exact
+            path="/auth"
+            element={!user ? <Auth /> : <Navigate to="/posts" replace />}
+          />
         </Routes>
       </Container>
     </BrowserRouter>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid, Grow } from "@mui/material";
+import { AppBar, Container, Grid, Grow, Paper, TextField } from "@mui/material";
 import Posts from "../Posts/Posts";
 import Form from "../Forms/Form";
 import { useDispatch } from "react-redux";
-import useStyles from "../../Styles";
 import { getPosts } from "../../actions/posts";
+import PaginationCard from "../Pagination/Pagination";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import ChipInput from "@mui/material/Chip";
+import useStyles from "./styles";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
+  //const classes = useStyles();
   const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useNavigate();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
 
   useEffect(() => {
     dispatch(getPosts());
@@ -17,18 +29,36 @@ const Home = () => {
   return (
     <div>
       <Grow in>
-        <Container>
+        <Container maxWidth="xl">
           <Grid
             container
             justify="space-between"
             alignItems="stretch"
             spacing={3}
+            // className={classes.gridContainer}
           >
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} sm={6} md={9}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppBar
+                // className={classes.appBarSearch}
+                position="static"
+                color="inherit"
+              >
+                <TextField
+                  name="search"
+                  variant="outlined"
+                  label="search Memories"
+                  fullWidth
+                  value="TEST"
+                  onChange={() => {}}
+                />
+              </AppBar>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
+              <Paper elevation={6}>
+                <PaginationCard />
+              </Paper>
             </Grid>
           </Grid>
         </Container>
